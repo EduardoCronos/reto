00b36085eecb0b62bb513f4c34814ba8a53c6238
EduardoCrono <anhatasesha@gmail.com>
2020-08-25 15:17:10 -0500

    Creacion estructura del proyecto

---
+++ spring-boot-reto/src/main/java/com/crono/reto/SpringBootRetoApplication.java
@@ -0,0 +1,13 @@
+package com.crono.reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRetoApplication.class, args);
+	}
+
+}
