b0d77569bfba9e69d61b10f4a2d636fce12ab56c
EduardoCrono <anhatasesha@gmail.com>
2020-08-27 00:35:02 -0500

    Modificaciones al aplication.properties

--- spring-boot-reto/src/main/java/com/crono/reto/repository/DeviceRepository.java
+++ spring-boot-reto/src/main/java/com/crono/reto/repository/DeviceRepository.java
@@ -1,9 +1,11 @@
 package com.crono.reto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.crono.reto.entity.Device;
 
+@Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
 	
 }
